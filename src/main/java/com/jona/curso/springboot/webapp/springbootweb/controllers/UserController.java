840a15d50993156a6dd8cb3e8ba91d024e358e3e
Jonathan Sánchez <jonulisanpar@gmail.com>
2024-01-26 01:31:45 -0600

    Pasar datos a a vista con Model y Thymeleaf

--- src/main/java/com/jona/curso/springboot/webapp/springbootweb/controllers/UserController.java
+++ src/main/java/com/jona/curso/springboot/webapp/springbootweb/controllers/UserController.java
@@ -1,13 +1,28 @@
 package com.jona.curso.springboot.webapp.springbootweb.controllers;
 
+import java.util.Map;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class UserController {
 
     @GetMapping("/details")
-    public String details() {
+    public String details(Model model) {
+        model.addAttribute("title", "Hola mundo Spring Boot");
+        model.addAttribute("name", "Jonathan");
+        model.addAttribute("lastname", "Sanchez");
         return "details";
     }
+
+    // Ejemplo usando Map
+    // @GetMapping("/details")
+    // public String details(Map<String, Object> model) {
+    //     model.put("title", "Hola mundo Spring Boot");
+    //     model.put("name", "Jonathan");
+    //     model.put("lastname", "Sanchez");
+    //     return "details";
+    // }
 }
