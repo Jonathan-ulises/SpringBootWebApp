a63a988959e8a6310ba842a853dad6fe357ccd2c
Jonathan Sánchez <jonulisanpar@gmail.com>
2024-01-26 01:35:09 -0600

    Elimina el import no usado

--- src/main/java/com/jona/curso/springboot/webapp/springbootweb/controllers/UserController.java
+++ src/main/java/com/jona/curso/springboot/webapp/springbootweb/controllers/UserController.java
@@ -1,7 +1,5 @@
 package com.jona.curso.springboot.webapp.springbootweb.controllers;
 
-import java.util.Map;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
