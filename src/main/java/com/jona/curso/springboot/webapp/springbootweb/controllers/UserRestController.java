afa2dfac35cddb8fc30c08beb5b44f4df29e9c9c
Jonathan Sánchez <jonulisanpar@gmail.com>
2024-01-27 02:24:43 -0600

    Respuesta de lista de usuarios.

--- src/main/java/com/jona/curso/springboot/webapp/springbootweb/controllers/UserRestController.java
+++ src/main/java/com/jona/curso/springboot/webapp/springbootweb/controllers/UserRestController.java
@@ -1,7 +1,9 @@
 package com.jona.curso.springboot.webapp.springbootweb.controllers;
 
 
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.springframework.web.bind.annotation.GetMapping;
@@ -23,6 +25,22 @@ public class UserRestController {
         userDto.setUser(user);
         userDto.setTitle("Hola Mundo Spring Boot");
         return userDto;
+    }
+
+    @GetMapping("/list")
+    public List<User> list() {
+        User user = new User("Jonathan", "Sanchez");
+        User user2 = new User("Oscar", "Alan");
+        User user3 = new User("Luis", "Aranda");
+        
+        List<User> users = Arrays.asList(user, user2, user3);
+
+        // List<User> users = new ArrayList<>();
+        // users.add(user);
+        // users.add(user2);
+        // users.add(user3);
+
+        return users;
     }
 
     // Forma con Map
