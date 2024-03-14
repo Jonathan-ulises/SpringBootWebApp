f8bf3895d24dd2778d55c6967ac5238ddc9c06f5
Jonathan Sánchez <jonulisanpar@gmail.com>
2024-03-14 12:46:34 -0600

    Configuracion de .properties personalizados en clase aparte de SpringbootWebApplication.

---
+++ src/main/java/com/jona/curso/springboot/webapp/springbootweb/ValuesConfig.java
@@ -0,0 +1,11 @@
+package com.jona.curso.springboot.webapp.springbootweb;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
+
+@Configuration
+@PropertySources({
+	@PropertySource("classpath:values.properties")
+})
+public class ValuesConfig { }
