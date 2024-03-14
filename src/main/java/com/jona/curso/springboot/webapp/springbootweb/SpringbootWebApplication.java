76ba24a679316813f00b3dbb8932732792ceb6f4
Jonathan Sánchez <jonulisanpar@gmail.com>
2024-03-14 12:43:15 -0600

    Configuracion de .properties personalizados.

--- src/main/java/com/jona/curso/springboot/webapp/springbootweb/SpringbootWebApplication.java
+++ src/main/java/com/jona/curso/springboot/webapp/springbootweb/SpringbootWebApplication.java
@@ -2,8 +2,15 @@ package com.jona.curso.springboot.webapp.springbootweb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 
 @SpringBootApplication
+// @PropertySources({
+// 	@PropertySource("classpath:values.properties"),
+// 	@PropertySource("classpath:values2.properties")
+// })
+@PropertySource("classpath:values.properties")
 public class SpringbootWebApplication {
 
 	public static void main(String[] args) {
